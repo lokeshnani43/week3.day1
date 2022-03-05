c02b88cc92c45764e07c9474794833ba676232fd
Lokesh Kumar <100150334+lokeshnani43@users.noreply.github.com>
2022-03-05 22:24:03 +0530

    Add files via upload

---
+++ FibbonacciSeries.java
@@ -0,0 +1,22 @@
+package week3.day1;
+
+public class FibbonacciSeries {
+
+	public static void main(String[] args) {
+
+		int range = 8, firstNum = 0, secNum = 1, sum;
+		
+		System.out.println(firstNum);
+		System.out.println(secNum);
+		
+		for (int i = 1; i < range; i++) {
+			
+			sum=firstNum+secNum;
+			firstNum=secNum;
+			secNum=sum;
+			System.out.println(sum);
+			
+		}
+			
+		}
+	}
